c4e48bda69e25d9488c22227ba59c0bd13663376
Louis Pan <louis@pan.me>
2018-03-19 05:53:00 +1100

    added minSafeInteger and safeModularIncrement/Decrement

--- src/JavaScript/Extras/Number.hs
+++ src/JavaScript/Extras/Number.hs
@@ -2,10 +2,24 @@
 
 module JavaScript.Extras.Number
     ( maxSafeInteger
+    , minSafeInteger
+    , safeModularIncrement
+    , safeModularDecrement
     ) where
 
 maxSafeInteger :: Int
 maxSafeInteger = js_maxSafeInteger
+
+minSafeInteger :: Int
+minSafeInteger = js_minSafeInteger
+
+-- | always returns a nubber between [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
+safeModularIncrement :: Int -> Int
+safeModularIncrement i = if i >= maxSafeInteger then minSafeInteger else i + 1
+
+-- | always returns a number between [Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]
+safeModularDecrement :: Int -> Int
+safeModularDecrement i = if i <= minSafeInteger then maxSafeInteger else i - 1
 
 #ifdef __GHCJS__
 
@@ -13,9 +27,16 @@ foreign import javascript unsafe
   "Number.MAX_SAFE_INTEGER"
   js_maxSafeInteger :: Int
 
+foreign import javascript unsafe
+  "Number.MIN_SAFE_INTEGER"
+  js_maxSafeInteger :: Int
+
 #else
 
 js_maxSafeInteger :: Int
 js_maxSafeInteger = 0
+
+js_minSafeInteger :: Int
+js_minSafeInteger = 0
 
 #endif
