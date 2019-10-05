70f1558ec9c01cc4dc9112fdd9d971666ed83937
Louis Pan <louis@pan.me>
2019-10-05 21:37:09 +1000

    moved example to separate cabal

--- example/Shakefile.hs
+++ example/Shakefile.hs
@@ -9,12 +9,17 @@ import Data.Maybe
 import Data.Typeable
 import Development.Shake
 import Development.Shake.FilePath
+import qualified Distribution.PackageDescription.Parsec as D
+import qualified Distribution.Pretty as D
+import qualified Distribution.Types.GenericPackageDescription as D
+import qualified Distribution.Types.PackageDescription as D
+import qualified Distribution.Types.PackageId as D
+import qualified Distribution.Verbosity as D
 import Safe
 import System.Directory
 import System.Info.Extra
 
-project = "javascript-extras"
-projectTarget = "test"
+project = "javascript-extras-example"
 
 -- | Unfortunately, Cabal is missing programatic querying of build outputs
 -- https://www.haskell.org/cabal/users-guide/nix-local-build.html#where-are-my-build-products
@@ -22,9 +27,9 @@ jsexePath ghcjsVersion projVersion = "dist-newstyle/build/x86_64-"
     <> if isWindows then "windows" else "linux"
     <> "/ghcjs-" <> ghcjsVersion <> "/"
     <> project <> "-" <> projVersion <> "/x/"
-    <> project <> "-" <> projectTarget <> "/build/"
-    <> project <> "-" <> projectTarget <> "/"
-    <> project <> "-" <> projectTarget <> ".jsexe"
+    <> project <> "/build/"
+    <> project <> "/"
+    <> project <> ".jsexe"
 
 -- | convert the jsexe all.js from ghcjs compilation into a node module
 jsexeModule str = "module.exports = function(){\n"
@@ -32,32 +37,20 @@ jsexeModule str = "module.exports = function(){\n"
     <> str
     <> "\n}\n"
 
-buildGhcjsPackageCmd pkg pkgTarget = do
-    pwd <- liftIO $ getCurrentDirectory
-    let parentWd = takeDirectory $ dropTrailingPathSeparator pwd
-    command_ [Cwd parentWd] "cabal" ["v2-build", "--ghcjs", pkg <> "-" <> pkgTarget]
-
 newtype GhcjsVersion = GhcjsVersion () deriving (Show, Typeable, Eq, Hashable, Binary, NFData)
 type instance RuleResult GhcjsVersion = String
 oracleGhcjsVersion = addOracle $ \(GhcjsVersion _) -> (trim . fromStdout) <$> cmd "ghcjs --numeric-version" :: Action String
 
-newtype ProjectDirectory = ProjectDirectory () deriving (Show, Typeable, Eq, Hashable, Binary, NFData)
-type instance RuleResult ProjectDirectory = String
--- | project directory is above this one
-oracleProjectDirectory = addOracle $ \(ProjectDirectory _) -> liftIO $ (takeDirectory . dropTrailingPathSeparator) <$> getCurrentDirectory
-
 newtype ProjectVersion = ProjectVersion () deriving (Show, Typeable, Eq, Hashable, Binary, NFData)
 type instance RuleResult ProjectVersion = String
-oracleProjectVersion getProjectDirectory = addOracle $ \(ProjectVersion _) -> do
-    projDir <- getProjectDirectory $ ProjectDirectory ()
-    Stdout out <- command [Cwd projDir] "grep"  ["^\\s*version:", project <.> "cabal"]
-    pure $ words out `at` 1
+oracleProjectVersion = addOracle $ \(ProjectVersion _) -> do
+    desc <- liftIO $ D.readGenericPackageDescription D.normal (project <.> "cabal")
+    pure $ D.prettyShow $ D.pkgVersion $ D.package $ D.packageDescription desc
 
 main :: IO ()
 main = shakeArgs shakeOptions $ do
     getGhcjsVersion <- oracleGhcjsVersion
-    getProjectDirectory <- oracleProjectDirectory
-    getProjectVersion <- oracleProjectVersion getProjectDirectory
+    getProjectVersion <- oracleProjectVersion
 
     want [build </> "hsMain.js"]
 
@@ -65,16 +58,15 @@ main = shakeArgs shakeOptions $ do
         putNormal $ "Deleting " <> build
         removeFilesAfter build ["//"]
 
-    project <> "-" <> projectTarget <> ".jsexe" </> "all.js" %> \_ -> do
+    project <> ".jsexe" </> "all.js" %> \_ -> do
          alwaysRerun
-         buildGhcjsPackageCmd project projectTarget
+         cmd_ "cabal" ["v2-build", "--ghcjs", project]
 
     build </> "hsMain.js" %> \out -> do
         ghcVer <- getGhcjsVersion $ GhcjsVersion ()
         projectVer <- getProjectVersion $ ProjectVersion ()
-        projectDir <- getProjectDirectory $ ProjectDirectory ()
         let jsexe = jsexePath ghcVer projectVer
-            alljs = projectDir </> jsexe </> "all.js"
+            alljs = jsexe </> "all.js"
         src <- readFile' alljs
         writeFile' out $ jsexeModule src
   where
